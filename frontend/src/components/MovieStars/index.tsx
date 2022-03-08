5c74dd73dcc4be738ecc1f250376bdaafc3b454a
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-03-08 10:09:10 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as StarFull} from 'assets/img/star-full.svg';
+import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
+
+import './style.css';
+
+function MovieStars(){
+    return(
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    );
+}
+
+export default MovieStars;
