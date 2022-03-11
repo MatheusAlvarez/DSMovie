2372421f48ae43ed24e22b35c08a8462fa2f458c
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-03-11 09:29:04 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,18 +1,19 @@
 import MovieStars from "components/MovieStars";
-
 import './style.css'
 
-function MovieScore(){
+type Props ={
+    score: number;
+    count: number;
+}
 
-    const score = 3.5;
-    const count = 13;
+function MovieScore( { score, count }: Props){
 
     return(
         <div className="dsmovie-score-container">
-    <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-    <MovieStars />
-    <p className="dsmovie-score-count">{count} avaliações</p>
-</div>
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars score={score}/>
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
     );
 }
 
